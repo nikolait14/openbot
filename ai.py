dcf75edd8d7c0060cc4121f91db89580fb60fe31
nikolai tyukavkin <nikolaitiukavkinfortiktok@gmail.com>
2025-06-05 03:22:29 +0300

    база для логики работы с опен api

--- ai.py
+++ ai.py
@@ -1,15 +1,19 @@
 from openai import OpenAI
 from tokens import open
+
+
 client = OpenAI(
   base_url="https://api.langdock.com/openai/eu/v1",
   api_key=open                                      # токен landgock'а из tokens.py
 )
 
-completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {"role": "user", "content": "Write a short poem about cats."}
-  ]
-)
 
-print(completion.choices[0].message.content)
+def request(req, model):
+    completion = client.chat.completions.create(
+      model=model,
+      messages=[
+        {f"role": "user", "content": {req}}
+      ]
+    )
+
+    print(completion.choices[0].message.content)
