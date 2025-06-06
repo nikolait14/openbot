05877a8083956f2f5fd527a8582ce6e91eef12c4
Egor <sergeev.ee@maail.ru>
2025-06-06 15:48:39 +0300

    удаление кнопок

--- bot.py
+++ bot.py
@@ -1,27 +1,32 @@
 import telebot
 from telebot import types
+from telebot.types import Message, InlineKeyboardMarkup as Ikm, InlineKeyboardButton as Ikb, CallbackQuery as Cq
 
 import tokens
 
 bot = telebot.TeleBot(tokens.token)
 
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("")
-    btn2 = types.KeyboardButton("")
+    markup = types.ReplyKeyboardMarkup(True,False)
+    btn1 = types.KeyboardButton("1")
+    btn2 = types.KeyboardButton("2")
     markup.add(btn1, btn2)
-    us = f"<b>Hi</b>,<b>{message.from_user.first_name}</b>"
+    id1=message.chat.id
+    print(id1)
+    us = f"<b>Hi</b>,<b>{message.from_user.first_name}!</b>\n<b>What you want to do?</b>"
     bot.send_message(message.chat.id, us, parse_mode='html', reply_markup=markup)
-
-
 @bot.message_handler()
 def func(message):
-    if message.text == "":
-        bot.send_message(message.chat.id, '<b>ПН-ПТ</b>-7:00-21:00\n<b>СБ-ВС</b>-9:00-19:00', parse_mode='html')
-    elif message.text == '':
-        bot.send_message(message.chat.id, "test", parse_mode='html')
+    if message.text == "1":
+        bot.send_message(message.chat.id, 'тест', parse_mode='html',reply_markup=types.ReplyKeyboardRemove())
+
+    elif message.text == '2':
+        bot.send_message(message.chat.id, "test", parse_mode='html',reply_markup=types.ReplyKeyboardRemove())
+
+
 
 
 bot.polling(none_stop=True)
